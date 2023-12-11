06b5780d804db54f90254de088c5eb199d33750b
Ekalavyaaaa <145014058+Ekalavyaaaa@users.noreply.github.com>
2023-12-11 18:18:29 +0530

    Def cubes_of_even_numbers_for_loop.py

--- Python files/def cubes_of_even_numbers_for_loop.py
+++ Python files/def cubes_of_even_numbers_for_loop.py
@@ -5,7 +5,6 @@ def cubes_of_even_numbers_for_loop(input_list):
             result.append(element**3)
     return result
 
-# Example usage:
 input_list = [7,45,6,79,99]
 result_for_loop = cubes_of_even_numbers_for_loop(input_list)
 print("Result using 'for' loop:", result_for_loop)
