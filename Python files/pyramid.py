92bf175756a2c6e4b169488266c9cb4454d0b160
Ekalavyaaaa <145014058+Ekalavyaaaa@users.noreply.github.com>
2023-12-11 18:26:03 +0530

    Pyramid Assignment.py

--- Python files/pyramid.py
+++ Python files/pyramid.py
@@ -1,21 +1,21 @@
 def print_pyramid(rows):
     for i in range(1, rows + 1):
-        # Print spaces
+    
         print(" " * (rows - i), end="")
-        # Print '*' characters
+        
         print("* " * i)
 
 def print_reverse_pyramid(rows):
     for i in range(rows, 0, -1):
-        # Print spaces
+        
         print(" " * (rows - i), end="")
-        # Print '*' characters
+        
         print("* " * i)
 
-# Input the number of rows for the pyramid
+
 num_rows = int(input("Enter the number of rows for the pyramid: "))
 
-# Print the pyramid
+
 print("Pyramid:")
 print_pyramid(num_rows)
 
